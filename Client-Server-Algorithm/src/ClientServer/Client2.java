1e56ec47cf4bd0f5ed6b048b7e65da5f9894fbd9
lehlohonolo <lehlohonolo@lehlohonolo-HP-EliteBook-2540p>
2021-08-16 19:05:45 +0200

    Surround outer code block with while loop.

---
+++ Client-Server-Algorithm/src/ClientServer/Client2.java
@@ -0,0 +1,11 @@
+package ClientServer;
+
+public class Client2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New prototype.");
+
+	}
+
+}

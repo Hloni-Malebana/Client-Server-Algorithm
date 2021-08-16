af90773629431e34b3fa068752e4fa0390d0512b
lehlohonolo <lehlohonolo@lehlohonolo-HP-EliteBook-2540p>
2021-08-16 19:14:42 +0200

    Surround out block with infinite while loop,
    still need to fix the issue that the code does not break when user
    enters the word BYE.

--- Client-Server-Algorithm/src/ClientServer/Client.java
+++ Client-Server-Algorithm/src/ClientServer/Client.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.Scanner;
-
+//Client Server Algorithm
 public class Client{
 	
 	public static void main(String [] args) {
